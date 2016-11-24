c7c7ca9809fdf2457a006b1e6c298a3800a7d784
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-11-24 10:48:59 +0000

    Basic setup of express application: bin/www, app, routes, views, debug,

---
+++ app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * Import npm packages
+ */
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+/**
+ * Import local libraries
+ */
+const routeUsers = require('./routes/users')
+
+/**
+ * Instantiate...
+ */
+const app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+/**
+ * Add Middlewares
+ */
+app.use(favicon(path.join(__dirname, 'public', 'R2D2.ico')))
+//app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+/**
+ * Add Routes
+ */
+app.use('/users', routeUsers)
+
+/**
+ * Error-handling middlewares
+ */
+// catch 404 and forward to default error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// print stacktrace
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    res.render('error', {
+        message: err.message,
+        error: err
+    })
+})
+
+module.exports = app
