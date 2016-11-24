972da8f84a25b98561cf18d353aaa621a9007265
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-11-24 17:08:03 +0000

    Add 2 endpoints: users/:id and /users/:id/teams
    * 2 additional views: edit and teams
    * Move index into views/users folder
    * Remove container from layout

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const usersDb = require('./usersDb')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Users listings' });
-});
+  res.render('users/index', { 
+    title: 'Users listings',
+    users: usersDb 
+  })
+})
 
-module.exports = router;
+router.get('/:id', function(req, res, next) {
+  const user = usersDb[req.params.id]
+  if(user == undefined) return next()
+  res.render('users/edit', { 
+    'title': 'edit',
+    'user': user,
+  })
+})
+
+router.get('/:id/teams', function(req, res, next) {
+  const user = usersDb[req.params.id]
+  res.render('users/teams', { 
+    title: user.username + 'Favourite Teams',
+    teams: user.teams 
+  })
+})
+
+
+module.exports = router
