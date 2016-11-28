d9e25d87c2662b787f9cd1f0d9d8b21c6986d23f
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-11-28 14:47:15 +0000

    Setup connectController. Include: * loadControllersFrom() main method - loads folder or controller object * parseMethodName() - Converts method name into a Route path

---
+++ tests/connectControllerTest.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const controller = require('./../lib/connectController')
+
+module.exports.testLoadControllers = function(test) {
+    controller(__dirname + './../controllers')
+    test.expect(0)
+    test.done()
+}
+
+module.exports.testParseMethodNameWithoutParameters = function(test) {
+    testParseMethodName(
+        function get_members() {}, // actual
+        '/members',                // expected 
+        test)
+}
+
+module.exports.testParseMethodNameWithoutParametersNorPrefix = function(test) {
+    testParseMethodName(
+        function members() {},   // actual
+        '/members',              // expected 
+        test)
+}
+
+module.exports.testParseMethodNameWithoutGetPrefix = function(test) {
+    testParseMethodName(
+        function index_id_members() {},   // actual
+        '/:id/members',                   // expected 
+        test)
+}
+
+module.exports.testParseMethodNameWithGet = function(test) {
+    testParseMethodName(
+        function get_index_id_members() {},   // actual
+        '/:id/members',                       // expected 
+        test)
+}
+
+function testParseMethodName(method, expected, test) {
+    /**
+     * Act
+     */
+    test.expect(1)
+    const routePath = controller.parseMethodName(method)
+    /**
+     * Assert
+     */
+    test.equal(routePath, expected)
+    test.done()   
+}
