71c2318a76396e7a28dc25f7a0ce487b96bdec53
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-12-23 16:01:28 +0000

    Load controller with specific name

--- tests/connectControllerLoadObjectTest.js
+++ tests/connectControllerLoadObjectTest.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const controller = require('./../index')
-const userCtr = require('./controllers/userController.js')
+const userCtr = require('./controllers/users.js')
 
 /**
  * mws is a Middleware of Middlewares (Router objects).
@@ -9,6 +9,8 @@ const userCtr = require('./controllers/userController.js')
  * In this case there is a single Router corresponding to userCtr source.
  * In turn, each Router contains a Middleware for each method of the controller. 
  */
-const mws = controller(userCtr)
+const mws = controller(
+    userCtr,
+    { name: 'users'} )
 
 module.exports = require('./units/connectControllerLoadTest')(mws)
