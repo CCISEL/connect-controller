7d3fd50e3817c040b092f440a432763dfa973287
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-01-11 13:56:41 +0000

    Improve unit tests with a connect server and testing locals variables

--- tests/controllers/users.js
+++ tests/controllers/users.js
@@ -3,14 +3,16 @@
 module.exports = {
     'groups':  function (nr, res) { res.render('viewName', nr) },
     'index': function () {return 'I am index'},
-    'index_teamId': function (teamId) {return teamId}, 
+    'index_teamId': function (teamId, reqVar, resLocalVar, appLocalVar) {
+        return { teamId, reqVar, resLocalVar, appLocalVar }
+    }, 
     'index_id_members':  function () {return 'I am index_id_members'},
     'dummy_bastof_nr_members':  function (nr) {return nr},
     'dummy_nr_teams_xpto_str':  function (nr, arg1, arg2, str) {return {
         'nr': nr, 'arg1': arg1, 'arg2': arg2, 'str': str
     }},
     'xone_stuff': function(stuff, req, next) {
-        next(req.params.stuff)
+        next()
     },
     'post_xone': function(stuff) {
         return stuff
