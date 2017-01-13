faf38abdcbd50884228b9ec813a4700fc3042881
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-01-13 16:49:39 +0000

    Remove useless wrapper function from modules controllers/football and routes/football

--- example/lib/controllers/football.js
+++ example/lib/controllers/football.js
@@ -1,50 +1,48 @@
 'use strict'
 
-module.exports = (function(){
-    const footballDb = require('./../db/footballDb')
-    return {
-        'leagues_id_table': leagues_id_table,          // binds to /leagues/:id/table
-        // <=> 'leagues_id_table': footballDb.leagueTable
-        'leagues': leagues,                            // binds to /leagues
-        'index': index                                 // binds to /
-    }
+const footballDb = require('./../db/footballDb')
 
-    /**
-     * Every action parameter (e.g. id) taking part of method's name (e.g. _id_)
-     * is bound to the corresponding argument of req.params (e.g. req.params.id).
-     * In this case this function is useless and we could simply bound 
-     * property 'leagues_id_table' to method footballDb.leagueTable.
-     */
-    function leagues_id_table(id){
-        return footballDb.leagueTable(id)
-    }
+module.exports = {
+    leagues_id_table, // binds to /leagues/:id/table
+    leagues,          // binds to /leagues
+    index             // binds to /
+}
+
+/**
+ * Every action parameter (e.g. id) taking part of method's name (e.g. _id_)
+ * is bound to the corresponding argument of req.params (e.g. req.params.id).
+ * In this case this function is useless and we could simply bound 
+ * property 'leagues_id_table' to method footballDb.leagueTable.
+ */
+function leagues_id_table(id){
+    return footballDb.leagueTable(id)
+}
+
+/**
+ * Every action parameter (e.g. name) that is NOT part of the method's name
+ * will be searched on req.query, req.body, req, res.locals and req.app.locals.
+ */
+function leagues(name) {
+    if(name) name = name.toLowerCase()
+    return footballDb
+        .leagues()
+        .then(leagues => leagues
+            .filter(l => !name || l.caption.toLowerCase().indexOf(name) >= 0)
+            .map(addLeaguePath))
     
-    /**
-     * Every action parameter (e.g. name) that is NOT part of the method's name
-     * will be searched on req.query, req.body, req, res.locals and req.app.locals.
-     */
-    function leagues(name) {
-        if(name) name = name.toLowerCase()
-        return footballDb
-            .leagues()
-            .then(leagues => leagues
-                .filter(l => !name || l.caption.toLowerCase().indexOf(name) >= 0)
-                .map(addLeaguePath))
-        
-        function addLeaguePath(league) {
-            league.leagueHref = "/football/leagues/" + league.id + "/table"
-            return league
-        }
+    function addLeaguePath(league) {
+        league.leagueHref = "/football/leagues/" + league.id + "/table"
+        return league
     }
+}
 
-    /**
-     * Whenever an action receives the `res` parameter, the connect-controller
-     * gets out of the way and delegates on that action the responsibility of
-     * sending the response.
-     * So whenever you want to do something different from the default behavior 
-     * you just have to append res to your parameters.
-     */
-    function index(res) {
-        res.redirect('/football/leagues')
-    }
-})()
+/**
+ * Whenever an action receives the `res` parameter, the connect-controller
+ * gets out of the way and delegates on that action the responsibility of
+ * sending the response.
+ * So whenever you want to do something different from the default behavior 
+ * you just have to append res to your parameters.
+ */
+function index(res) {
+    res.redirect('/football/leagues')
+}
--- example/lib/routes/football.js
+++ example/lib/routes/football.js
@@ -3,44 +3,41 @@
 const footballDb = require('./../db/footballDb')
 const express = require('express')
 
-module.exports = (function(){
-    const router = express.Router()    
-    router.get('/leagues/:id/table', leagues_id_table)
-    router.get('/leagues', leagues)
-    router.get('/', index)
-    return router
+const router = express.Router()
+router.get('/leagues/:id/table', leagues_id_table)
+router.get('/leagues', leagues)
+router.get('/', index)
+module.exports = router
 
-    function leagues_id_table(req, res, next) {
-        const id = req.params.id
-        footballDb
-            .leagueTable(id)
-            .then(league => {
-                res.render('football/leagues/table', league)
-            })
-            .catch(err => next(err))
+function leagues_id_table(req, res, next) {
+    const id = req.params.id
+    footballDb
+        .leagueTable(id)
+        .then(league => {
+            res.render('football/leagues/table', league)
+        })
+        .catch(err => next(err))
+}
+
+function leagues(req, res, next) {
+    let name
+    if(req.query.name) name = req.query.name.toLowerCase()
+    footballDb
+        .leagues()
+        .then(leagues => {
+            leagues = leagues
+                .filter(l => !name || l.caption.toLowerCase().indexOf(name) >= 0)
+                .map(addLeaguePath)
+            res.render('football/leagues', leagues)
+        })
+        .catch(err => next(err))
+
+    function addLeaguePath(league) {
+        league.leagueHref = "/router/leagues/" + league.id + "/table"
+        return league
     }
+}
 
-    function leagues(req, res, next) {
-        let name
-        if(req.query.name) name = req.query.name.toLowerCase()
-        footballDb
-            .leagues()
-            .then(leagues => {
-                leagues = leagues
-                    .filter(l => !name || l.caption.toLowerCase().indexOf(name) >= 0)
-                    .map(addLeaguePath)
-                res.render('football/leagues', leagues)
-            })
-            .catch(err => next(err))
-
-        function addLeaguePath(league) {
-            league.leagueHref = "/router/leagues/" + league.id + "/table"
-            return league
-        }
-    }
-
-    function index(req, res, next) {
-        res.redirect('/router/leagues')
-    }
-
-})()
+function index(req, res, next) {
+    res.redirect('/router/leagues')
+}
